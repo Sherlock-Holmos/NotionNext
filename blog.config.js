a3a247329d28c21bda7af33f9cd6c69d42ea6998
JokerSH <69100985+Sherlock-Holmos@users.noreply.github.com>
2023-04-02 01:11:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Holmesbro', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '嗑一颗红色药丸，重启矩阵💊', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.holmesbro.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'www.holmesbro.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '2783573891@qq.com', // 邮箱地址 例如mail@tangly1024.com
