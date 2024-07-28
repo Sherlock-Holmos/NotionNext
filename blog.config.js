10471b0e8a6f6209ca724245cf7f727349666255
JokerSH <69100985+Sherlock-Holmos@users.noreply.github.com>
2024-07-28 10:11:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -322,13 +322,6 @@ const BLOG = {
       url: 'https://music.163.com/song/media/outer/url?id=29004400.mp3',
       cover:
         'http://p2.music.126.net/_49Xz_x9kTTdEgmYYk6w2w==/6672936069046297.jpg?param=130y130'
-    },
-    {
-      name: 'Shape of You',
-      artist: 'Ed Sheeran',
-      url: 'https://down-load.lanrar.com/file/?AmRTbQ4/VGVVXAc/UWRcMFZpUGhSXgFrAGZaLAI7BCUJZlVkDXBVWFFoBiUFf10xUXtQZgR9AWUBb1owAjtRDwJrU2AOblQxVToHYFEyXG1WNVBkUjwBMQAhWjoCLQQ4CTpVNQ1kVTFRMQZmBWldelF7UCUEZgExATZabgJqUX8CMlMzDnxUMlUyB35RZFxpVjhQYVI1ATUAZFppAjgEMgltVWMNNVVkUT8GZQVmXThRaVA2BGkBZgFnWmgCblFjAjZTNA42VDRVYgc3UStcMFZ8UGZSKwF2AHRaOQIsBGwJbVU/DWFVNVE3BmMFZl1qUT9QcwQvAWoBa1o5AjhRbQIzUzMOYVQxVTYHY1E9XGhWP1BhUiMBLQAhWjoCMgRyCTRVMg12VXRRdwYnBWxdbVE8UGcEagE2ATRaawJuUWgCM1MiDiZUaVVzB2xRN1xvVj9Qe1I7ATYAKVptAmgEPQknVTMNY1UxUSkGdgU1XTNRfFA7BAQBYAFtWmECbg==',
-      cover:
-        'http://p1.music.126.net/ZAbThPRnV0wVu2er0PIlvA==/109951168805157389.jpg?param=130y130'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
